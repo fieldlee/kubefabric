c382494d04585a2ef7c8902a75d15824edc274d7
fieldlee <ldpfieldlee@gmail.com>
2019-10-15 18:07:28 +0800

    add cli

--- go.mod
+++ go.mod
@@ -5,13 +5,11 @@ go 1.12
 require (
 	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/imdario/mergo v0.3.8 // indirect
-	golang.org/x/crypto v0.0.0-20190911031432-227b76d455e7 // indirect
-	golang.org/x/net v0.0.0-20190912160710-24e19bdeb0f2 // indirect
+	github.com/urfave/cli v1.22.1
+	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
-	golang.org/x/sys v0.0.0-20190913121621-c3b328c6e5a7 // indirect
 	golang.org/x/time v0.0.0-20190921001708-c4c64cad1fd0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
-	gopkg.in/yaml.v2 v2.2.2 // indirect
 	k8s.io/api v0.0.0-20190409021203-6e4e0e4f393b
 	k8s.io/apimachinery v0.0.0-20190404173353-6a84e37a896d
 	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
--- main.go
+++ main.go
@@ -1,14 +1,58 @@
 package main
 
 import (
-	"client-go/k8s"
 	"fmt"
+	"github.com/urfave/cli"
+	"log"
+	"os"
+	"kubefabric/kubeutils"
 )
 
 func main() {
 
-	kubeClient := k8s.InitClient()
+	app := cli.NewApp()
 
+	app.Flags = []cli.Flag {
+		cli.StringFlag{
+			Name: "action",
+			Value: "show",
+			Usage: "action for kubernetes",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		name := "show"
+		if c.NArg() > 0 {
+			name = c.Args().Get(0)
+		}
+		switch name {
+		case "show":
+			ShowKube()
+			break
+		case "create":
+			CreateKube()
+			ListKube()
+			break
+		case "update":
+			UpdateKube()
+			ListKube()
+			break
+		case "delete":
+			DeleteKube()
+			ListKube()
+			break
+		}
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ShowKube(){
+	kubeClient := kubeutils.InitClient()
 	podList := kubeClient.GetPod("")
 
 	for _,pod := range podList{
@@ -17,24 +61,28 @@ func main() {
 
 	nodeList := kubeClient.GetNode()
 
-	fmt.Println()
+	fmt.Println("===========================================================")
 	for _,node := range nodeList{
 		fmt.Printf("node name :%s node type:%s \n",node.Name,node.Kind)
 	}
-	// Deployment
+}
+func CreateKube(){
+	kubeClient := kubeutils.InitClient()
 	deployment,err := kubeClient.CreateDeployment()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	fmt.Println(deployment.Namespace,deployment.Name)
-
-	// Update Deployment
-	//err := kubeClient.UpdateDeployment("demo-deployment")
-	//if err != nil {
-	//	fmt.Println(err.Error())
-	//}
-	// List Deployment
-
+}
+func UpdateKube(){
+	kubeClient := kubeutils.InitClient()
+	err := kubeClient.UpdateDeployment("demo-deployment")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+func ListKube(){
+	kubeClient := kubeutils.InitClient()
 	listDeployment,err := kubeClient.ListDeployment()
 	if err != nil {
 		fmt.Println(err.Error())
@@ -42,10 +90,11 @@ func main() {
 	for _,deploy:=range listDeployment{
 		fmt.Println(deploy.Namespace,deploy.Name)
 	}
-	// Delete Deployment
-
-	//err = kubeClient.DeleteDeployment("demo-deployment")
-	//if err != nil {
-	//	fmt.Println(err.Error())
-	//}
+}
+func DeleteKube(){
+	kubeClient := kubeutils.InitClient()
+	err := kubeClient.DeleteDeployment("demo-deployment")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
